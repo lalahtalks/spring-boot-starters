f700766c8724c53a4426e18b140137685e654eab
dlesignac <damien.lesignac@dreev.com>
2021-05-27 22:55:19 +0200

    add ProblemType#toProblem()

--- problem-spring-boot-starter/src/main/java/io/lalahtalks/spring/problem/ProblemType.java
+++ problem-spring-boot-starter/src/main/java/io/lalahtalks/spring/problem/ProblemType.java
@@ -1,11 +1,30 @@
 package io.lalahtalks.spring.problem;
 
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+
 import java.net.URI;
 
 public interface ProblemType {
 
+    URI getType();
+
+    String getTitle();
+
     int getHttpStatusCode();
 
-    URI getType();
+    default Problem toProblem(String detail) {
+        var status = Status.valueOf(getHttpStatusCode());
+        return Problem.builder()
+                .withType(getType())
+                .withTitle(getTitle())
+                .withStatus(status)
+                .withDetail(detail)
+                .build();
+    }
+
+    default Problem toProblem() {
+        return toProblem(null);
+    }
 
 }
